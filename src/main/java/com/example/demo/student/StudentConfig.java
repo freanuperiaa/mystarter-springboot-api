8bc829a7e40fd0e0ebc2fe9a99e08cd7381ce121
Freanu Peria <magnussonfrancis@gmail.com>
2022-03-15 16:14:25 +0800

    everything's working fine, JPA and commandlinerunner is working okay.

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,37 @@
+package com.example.demo.student;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student mariam = new Student(
+                    "Mariam",
+                    "Mariam.jamal@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5),
+                    21
+            );
+            Student freanu = new Student(
+                    "Freanu",
+                    "freanu.jamal@gmail.com",
+                    LocalDate.of(1997, Month.AUGUST, 6),
+                    21
+            );
+
+            repository.saveAll(
+                    List.of(mariam, freanu)
+            );
+        };
+    }
+
+}
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -10,22 +11,15 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        Student mariam = new Student(
-                1L,
-                "Mariam",
-                "Mariam.jamal@gmail.com",
-                LocalDate.of(2000, Month.JANUARY, 5),
-                21
-        );
-        Student freanu = new Student(
-                2L,
-                "Freanu",
-                "freanu.jamal@gmail.com",
-                LocalDate.of(1997, Month.AUGUST, 6),
-                21
-        );
-        return List.of(mariam, freanu);
+        return studentRepository.findAll();
     }
 
 }
